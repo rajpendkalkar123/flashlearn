7c9f28279aec521a50135fa5800959313cb58f49
rajpendkalkar123 <raj.232313105@vcet.edu.in>
2025-03-23 01:04:31 +0530

    23 march

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart'; // Import dotenv
 import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/manual_quiz_screen.dart';
